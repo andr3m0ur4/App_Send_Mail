3b4e05da43cab1d8cae1244159a3900ff1912b4c
André Moura <andre.benedicto@etec.sp.gov.br>
2020-03-22 01:13:56 -0300

    Arquivos iniciais

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+		<meta charset="utf-8" />
+    	<title>App Send Mail</title>
+    	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+	</head>
+
+	<body>
+
+		<div class="container">  
+
+			<div class="py-3 text-center">
+				<img class="d-block mx-auto mb-2" src="logo.png" alt="" width="72" height="72">
+				<h2>Send Mail</h2>
+				<p class="lead">Seu app de envio de e-mails particular!</p>
+			</div>
+
+      		<div class="row">
+      			<div class="col-md-12">
+  				
+					<div class="card-body font-weight-bold">
+						<form>
+							<div class="form-group">
+								<label for="para">Para</label>
+								<input type="text" class="form-control" id="para" placeholder="joao@dominio.com.br">
+							</div>
+
+							<div class="form-group">
+								<label for="assunto">Assunto</label>
+								<input type="text" class="form-control" id="assunto" placeholder="Assundo do e-mail">
+							</div>
+
+							<div class="form-group">
+								<label for="mensagem">Mensagem</label>
+								<textarea class="form-control" id="mensagem"></textarea>
+							</div>
+
+							<button type="submit" class="btn btn-primary btn-lg">Enviar Mensagem</button>
+						</form>
+					</div>
+				</div>
+      		</div>
+      	</div>
+
+	</body>
+</html>
